f01d0b0fa58b1742a22e643fcfd4188285e4745a
Moaz-Amr <MoazAmrAlsaid@gmail.com>
2025-02-04 18:54:49 +0200

    frontend and backend

--- project.py
+++ project.py
@@ -6,6 +6,26 @@ window = Tk()
 window.geometry('800x400')
 
 
+def end():
+    window.destroy()
+    
+def check():
+    user_input = user_entry.get().strip()  
+    
+    with open('D:\\mrmaz\\Documents\\SWE Orange\\day3\\Boycott Brands.txt') as file:
+        content = file.readlines()  
+        content = [line.strip() for line in content] 
+        
+        
+        for brand in content:
+            if user_input.lower() == brand.lower():  
+                title_2['text'] = 'Result: dont  buy from this brand!'
+                title_2['fg'] = 'red'
+                return
+                
+        title_2['text'] = 'Result: buy from this brand!'
+        title_2['fg'] = 'green'
+
 title_1 = Label(window, text='Enter Brand Name', fg='red',
                 font=('times new roman', 25))
 title_1.place(x=280, y=60)
@@ -20,11 +40,11 @@ user_entry = Entry(window, fg='black', bg='yellow',
 user_entry.place(x=250, y=100)
 
 check_button = Button(window, text='Check', bg='yellow',
-                font=('times new roman', 25))
+                font=('times new roman', 25), command=check)
 check_button.place(x=280, y=250)
 
 exit_button = Button(window, text='Exit', bg='green',
-                font=('times new roman', 25))
+                font=('times new roman', 25), command=end)
 exit_button.place(x=500, y=250)
 
 nitemode_button = Button(window, text='Nite Or Light Mode', bg='black',fg='white',
